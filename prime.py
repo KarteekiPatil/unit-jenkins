36392ebb7448b411b7dbe0fbfad1554620a720c5
karteekipatil27 <145987634+karteekipatil27@users.noreply.github.com>
2023-10-10 10:15:07 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -3,14 +3,6 @@ import math
 def is_prime(num):
      if n < 0:
         return 'Negative numbers are not allowed'
-    if n <= 1:
-        return False
-
-    if n == 2:
-        return True
-
-    if n % 2 == 0:
-        return False
     for i in range(2,int(math.sqrt(num))+1):
          if num%i==0:
               return False
